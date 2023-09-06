8ea915c37b05949828b7b3db64359e45b905228c
Francisca Cardemil <fr.cardemil@duocuc.cl>
2023-09-05 21:24:27 -0300

    Cambios 1/9

--- PROYECTO UmamiUrbano/src/app/pages/home/home.page.ts
+++ PROYECTO UmamiUrbano/src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewChildren } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AnimationController, IonCard } from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  data:any;
+  private animation!: Animation;
+  constructor(private activateRoute: ActivatedRoute, private router: Router, private animationController:AnimationController) {
+    this.activateRoute.queryParams.subscribe(params=>{
+      if(this.router.getCurrentNavigation()?.extras.state){
+        this.data=this.router.getCurrentNavigation()?.extras.state?.["user"];
+        console.log(this.data)
+      }else{
+        this.router.navigate(["/login"]);
+      }
+    });
+  }
+  @ViewChild('animar1', {read: ElementRef, static: true})
+  animar1!: ElementRef;
 
+  ngAfterViewInit(){
+    const animacion1=this.animationController
+    .create()
+    .addElement(this.animar1.nativeElement)
+    .duration(3000)
+    .iterations(Infinity)
+    .keyframes([
+      {offset: 0, background: 'blue'},
+      {offset: 0.72, background: 'var(--background)'},
+      {offset: 1, background: 'red'}
+  ]);
+
+    this.animation=this.animationController
+    .create()
+    .duration(3000)
+    .iterations(Infinity)
+    .addAnimation([animacion1]);
+
+  this.animation.play();
+  }
 }
--- PROYECTO UmamiUrbano/src/app/pages/login/login.page.ts
+++ PROYECTO UmamiUrbano/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user={
+    usuario:"",
+    password:""
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  ingresar(){
+    console.log(this.user)
+    let navigationextras: NavigationExtras={
+      state:{
+        user: this.user
+      }
+    }
+    this.router.navigate(["/home"], navigationextras);
+  }
 }
