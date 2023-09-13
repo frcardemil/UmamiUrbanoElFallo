d188d256b97fca8fa695b03ab7dd40fc884b02b3
Francisca Cardemil <107263145+frcardemil@users.noreply.github.com>
2023-09-13 12:48:49 -0300

    Update login.page.ts

--- PROYECTO UmamiUrbano/src/app/pages/login/login.page.ts
+++ PROYECTO UmamiUrbano/src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { ToasController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -13,21 +14,48 @@ export class LoginPage implements OnInit {
     password:""
   }
 
-  constructor(private router: Router) { }
+  field:string="";
+
+  constructor(private router: Router, public toastController:ToastController) { }
 
   ngOnInit() {
   }
 
   ingresar(){
     console.log(this.user)
-    let navigationextras: NavigationExtras={
+    if (this.validateModel(this.user)) {
+      this.presentToast("top", "Bienvenido "+this.user.usuario);
+      let navigationextras: NavigationExtras={
       state:{
         user: this.user
       }
     }
     this.router.navigate(["/home"], navigationextras);
+  }else{
+      this.presentToast("bottom","Falta rellenar "+ this.field, 3500);
+  }
   }
 
+  validateModel(model:any){
+    for (var [key, value] of Object.entries(model)){
+      if (value=="") {
+      this.field=key;
+    return false;
+    }
+  }
+  return true;
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', message:string, duration?:number) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: duration?duration:2000,
+      position: position,
+    });
+
+    await toast.present();
+  }
+  
   registro(){
     this.router.navigate(["/recuperar"])
   }
